ee457157dad09a8d36ed7f688271e87aa5ccd8c1
sepehr <seeetayeshgholami85@gmail.com>
2024-07-23 12:50:44 -0700

    sign in back created successfully

---
+++ server/signinback.php
@@ -0,0 +1,31 @@
+<?php
+
+include("../template/connect.php");
+
+
+
+$name = $_POST['name'];
+$lastname = $_POST['lastname'];
+$pasword = $_POST['password'];
+$usernam = $_POST['username'];
+$phone = $_POST['phone'];
+$repeatpassword = $_POST['repeatpassword']
+
+
+
+
+
+  $sql = "INSERT INTO user (name, lastname, password, username, phone, repeatpassword)
+VALUES ('$name', '$lastname', '$pasword', '$usernam', '$phone', '$repeatpassword')";
+
+
+
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+
+?>
