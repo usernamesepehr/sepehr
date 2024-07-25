364069ebf84e27c9a845c0b3db75a6f17545b6be
sepehr <seeetayeshgholami85@gmail.com>
2024-07-24 21:32:59 -0700

    loginback created

---
+++ server/loginback.php
@@ -0,0 +1,26 @@
+<?php
+
+include("../template/connect.php");
+
+
+
+session_start();
+
+$phone = $_POST['phone'];
+$pasword = $_POST['password'];
+
+$sql = "SELECT id FROM user Where phone = '$phone' and password = '$pasword'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+    $_SESSION["id"] = $row['id'];
+     
+    header("Location: href:../panel/dashbord.html");
+
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
