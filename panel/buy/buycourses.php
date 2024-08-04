8790964f6f552101e096a479074bdec10acecdc6
sepehr <seeetayeshgholami85@gmail.com>
2024-08-03 23:05:47 -0700

    buy courses page created

---
+++ panel/buy/buycourses.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html dir="rtl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>buy courses</title>
+</head>    
+<body>
+
+<h2>welcome to buy courses page </h2>
+
+<?php
+
+include("../../server/buy/buycorsesback.php")
+
+?>
+
+</body>
+</html>
